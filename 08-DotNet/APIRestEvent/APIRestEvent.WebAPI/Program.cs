c3c5e8fc17cca7ba656ce1ea1d5dba04c5291ba4
Sergio Gordillo <sergiogordillodev@gmail.com>
2024-10-13 23:13:45 +0200

    ConnectionToSQLServerAndDBContextDone

--- 08-DotNet/APIRestEvent/APIRestEvent.WebAPI/Program.cs
+++ 08-DotNet/APIRestEvent/APIRestEvent.WebAPI/Program.cs
@@ -1,8 +1,18 @@
+using APIRestEvent.WebAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<ApplicationDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString"));
+});
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
