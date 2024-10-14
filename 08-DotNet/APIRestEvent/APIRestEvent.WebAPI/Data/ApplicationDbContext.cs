361a021dbe99e8d993b466c95227a216c5a6c9df
Sergio Gordillo <sergiogordillodev@gmail.com>
2024-10-14 23:17:41 +0200

    all needed to create db with entityframework done

--- 08-DotNet/APIRestEvent/APIRestEvent.WebAPI/Data/ApplicationDbContext.cs
+++ 08-DotNet/APIRestEvent/APIRestEvent.WebAPI/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using APIRestEvent.WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace APIRestEvent.WebAPI.Data
 {
@@ -9,5 +10,25 @@ namespace APIRestEvent.WebAPI.Data
         {
         }
 
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Participant> Participants { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Event>()
+                .HasMany(e => e.Participants)
+                .WithMany(p => p.Events);
+
+            modelBuilder.Entity<Event>()
+                .Property(e => e.StartDate)
+                .HasColumnType("date");
+
+            modelBuilder.Entity<Event>()
+                .Property(e => e.EndDate)
+                .HasColumnType("date")
+                .IsRequired(false)
+                .HasDefaultValue(null)
+                .HasAnnotation("CheckConstraint", "EndDate >= StartDate");
+        }
     }
 }
