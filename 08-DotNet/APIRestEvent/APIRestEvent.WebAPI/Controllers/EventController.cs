25cd16c89031ead7b47d03f87b73daf73bc78ef3
Sergio Gordillo <sergiogordillodev@gmail.com>
2024-10-15 12:38:44 +0200

    getControllerDone

---
+++ 08-DotNet/APIRestEvent/APIRestEvent.WebAPI/Controllers/EventController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using APIRestEvent.WebAPI.Models;
+using System;
+using APIRestEvent.WebAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIRestEvent.WebAPI.Controllers
+
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EventController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: /api/event
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Event>>> GetEventos()
+        {
+            var events = await _context.Events.ToListAsync();
+            return Ok(events);
+        }
+    }
+}
