273aa8efece25d107d0507b4233a7981aa04a083
Sergio Gordillo <sergiogordillodev@gmail.com>
2024-10-31 23:56:30 +0100

    working on new mappers

--- 08-DotNet/APIRestEvent/APIRestEvent.WebAPI/Mapping/MappingExtensions.cs
+++ 08-DotNet/APIRestEvent/APIRestEvent.WebAPI/Mapping/MappingExtensions.cs
@@ -6,53 +6,53 @@ public static class MappingExtensions
     // Mappers de entidad a DTO
     public static EventDTO ToDto(this Event eventEntity)
     {
-
         if (eventEntity == null) return null;
 
         return new EventDTO(
-           eventEntity.Id,
-           eventEntity.Name,
-           eventEntity.StartDate,
-           eventEntity.EndDate,
-           eventEntity.Description,
-           (eventEntity.Participants ?? new List<Participant>()).Select(p => p.ToDto()).ToList()
+            eventEntity.Id,
+            eventEntity.Name,
+            eventEntity.StartDate,
+            eventEntity.EndDate,
+            eventEntity.Description,
+            eventEntity.Participants?.Select(p => p.ToBaseDto()).ToList() ?? new List<BaseParticipantDTO>()
         );
     }
 
     public static ParticipantDTO ToDto(this Participant participantEntity)
     {
+        if (participantEntity == null) return null;
+
         return new ParticipantDTO(
-           participantEntity.Id,
-           participantEntity.Name,
-           participantEntity.LastName,
-           participantEntity.Email,
-           (participantEntity.Events ?? new List<Event>()).Select(p => p.ToDto()).ToList()
+            participantEntity.Id,
+            participantEntity.Name,
+            participantEntity.LastName,
+            participantEntity.Email,
+            participantEntity.Events?.Select(e => e.ToBaseDto()).ToList() ?? new List<BaseEventDTO>()
         );
     }
 
-    // Mappers de DTO a entidad
-    public static Event ToEntity(this EventDTO eventDTO)
+    public static BaseEventDTO ToBaseDto(this Event eventEntity)
     {
-        return new Event
-        {
-            Id = eventDTO.Id,
-            Name = eventDTO.Name,
-            StartDate = eventDTO.StartDate,
-            EndDate = eventDTO.EndDate,
-            Description = eventDTO.Description,
-            Participants = eventDTO.Participants.Select(p => p.ToEntity()).ToList()
-        };
+        if (eventEntity == null) return null;
+
+        return new BaseEventDTO(
+            eventEntity.Id,
+            eventEntity.Name,
+            eventEntity.StartDate,
+            eventEntity.EndDate,
+            eventEntity.Description
+        );
     }
 
-    public static Participant ToEntity(this ParticipantDTO participantDTO)
+    public static BaseParticipantDTO ToBaseDto(this Participant participantEntity)
     {
-        return new Participant
-        {
-            Id = participantDTO.Id,
-            Name = participantDTO.Name,
-            LastName = participantDTO.LastName,
-            Email = participantDTO.Email,
-            Events = participantDTO.Events.Select(e => e.ToEntity()).ToList()
-        };
+        if (participantEntity == null) return null;
+
+        return new BaseParticipantDTO(
+            participantEntity.Id,
+            participantEntity.Name,
+            participantEntity.LastName,
+            participantEntity.Email
+        );
     }
 }
