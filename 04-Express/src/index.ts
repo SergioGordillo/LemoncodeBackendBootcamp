de28ad50630fb5f16dadc17c36f9939307e42a79
Sergio Gordillo <sergiogordillodev@gmail.com>
2024-11-05 23:24:07 +0100

    getByIdMockBooks added

--- 04-Express/src/index.ts
+++ 04-Express/src/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { getBookList } from "./mock-db.js";
+import { getBookList, getBook } from "./mock-db.js";
 
 const app = express();
 
@@ -10,6 +10,13 @@ app.get("/", (req, res) => {
 app.get("/api/books", async (req, res) => {
   const bookList = await getBookList();
   res.send(bookList);
+});
+
+app.get("/api/books/:id", async (req, res) => {
+  const { id } = req.params;
+  const bookId = Number(id);
+  const book = await getBook(bookId);
+  res.send(book);
 });
 
 app.listen("3000", () => {
