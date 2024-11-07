15060c9c57486fbb36b34953e0de69deb2ea595b
Sergio Gordillo <sergiogordillodev@gmail.com>
2024-11-07 23:22:47 +0100

    put apiBooks done

--- 04-Express/src/index.ts
+++ 04-Express/src/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { getBookList, getBook, insertBook } from "./mock-db.js";
+import { getBookList, getBook, insertBook, updateBook } from "./mock-db.js";
 
 const app = express();
 app.use(express.json());
@@ -24,6 +24,14 @@ app.post("/api/books", async (req, res) => {
   const bookRequest = req.body;
   const newBook = await insertBook(bookRequest);
   res.status(201).send(newBook);
+});
+
+app.put("/api/books/:id", async (req, res) => {
+  const { id } = req.params;
+  const bookId = Number(id);
+  const updatedBook = req.body;
+  await updateBook(bookId, updatedBook);
+  res.sendStatus(204);
 });
 
 app.listen("3000", () => {
