96524a1f0e885151045f9043478dc51f82657f16
Sergio Gordillo <sergiogordillodev@gmail.com>
2024-11-10 23:28:25 +0100

    project ready to serve static files

--- 04-Express/src/index.ts
+++ 04-Express/src/index.ts
@@ -1,12 +1,13 @@
 import express from "express";
+import path from "path";
+import url from "url";
 import { booksAPI } from "./books.api.js";
 
 const app = express();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("My web works!!!");
-});
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+app.use(express.static(path.resolve(__dirname, "../public")));
 
 app.use("/api/books", booksAPI);
 
