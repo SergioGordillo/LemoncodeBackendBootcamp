75ae8dbf253f114ba646c7f03a6de19326e0e6cb
Sergio Gordillo <sergiogordillodev@gmail.com>
2024-11-29 21:13:12 +0100

    console runner changed

--- 04-Express/back/src/console-runners/index.ts
+++ 04-Express/back/src/console-runners/index.ts
@@ -1,19 +1,7 @@
 import inquirer from "inquirer";
 
-let exit = false;
-
-while(!exit){
-    const answer = await inquirer.prompt({
-        name: "consoleRunner",
-        type: 'list',
-        message: "Which console-runner do you want to run?",
-        choices: ["create-admin", "exit"]
-    });
-
-    if(answer.consoleRunner!=="exit"){
-        console.log("Create admin runner");
-    }else{
-        exit=true;
-    }
-}
-
+const answer = await inquirer.prompt({
+  name: "user",
+  type: "input",
+  message: "User name:",
+});
