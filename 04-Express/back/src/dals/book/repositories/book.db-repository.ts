d020b29180e5a810b8f81f574c9a0ab7ea29ca2b
Sergio Gordillo <sergiogordillodev@gmail.com>
2024-12-09 00:23:44 +0100

    pagination added to getBookList and saveBook implemented

--- 04-Express/back/src/dals/book/repositories/book.db-repository.ts
+++ 04-Express/back/src/dals/book/repositories/book.db-repository.ts
@@ -3,14 +3,27 @@ import { BookRepository } from "./book.repository.js";
 import { Book } from "../book.model.js";
 
 export const dbRepository: BookRepository = {
-  getBookList: async () => {
-    return await db.collection<Book>("books").find().toArray();
+  getBookList: async (page?: number, pageSize?: number) => {
+    const effectivePage = page ?? 1;
+    const effectivePageSize = pageSize ?? 10;
+    const skip = Boolean(page) ? (effectivePage - 1) * effectivePageSize : 0;
+    const limit = pageSize ?? 0;
+    return await db
+      .collection<Book>("books")
+      .find()
+      .skip(skip)
+      .limit(limit)
+      .toArray();
   },
   getBook: async (id: string) => {
     throw new Error("Not implemented");
   },
   saveBook: async (book: Book) => {
-    throw new Error("Not implemented");
+    const { insertedId } = await db.collection<Book>("books").insertOne(book);
+    return {
+      ...book,
+      _id: insertedId,
+    };
   },
   deleteBook: async (id: string) => {
     throw new Error("Not implemented");
