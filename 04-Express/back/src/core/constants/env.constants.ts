464a588a482145aacb9dd9760069e8d9caa9e6ba
Sergio Gordillo <sergiogordillodev@gmail.com>
2024-12-21 00:05:28 +0100

    fix: port environment constants

--- 04-Express/back/src/core/constants/env.constants.ts
+++ 04-Express/back/src/core/constants/env.constants.ts
@@ -4,7 +4,7 @@ dotenv.config();
 
 export const envConstants = {
   isProduction: process.env.NODE_ENV === "development",
-  PORT: process.env.PORT,
+  PORT: Number(process.env.PORT),
   STATIC_FILES_PATH: process.env.STATIC_FILES_PATH || "public",
   CORS_ORIGIN: process.env.CORS_ORIGIN,
   CORS_METHOD: process.env.CORS_METHOD,
