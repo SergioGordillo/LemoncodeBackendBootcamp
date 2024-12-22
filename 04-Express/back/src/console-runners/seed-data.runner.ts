2a6ec0a6e805f6b02cb05c232f15323670501cea
Sergio Gordillo <sergiogordillodev@gmail.com>
2024-12-22 23:49:30 +0100

    feat: changes on consolerunners

--- 04-Express/back/src/console-runners/seed-data.runner.ts
+++ 04-Express/back/src/console-runners/seed-data.runner.ts
@@ -1,33 +1,6 @@
-import prompts from 'prompts';
-import { runCommand } from './console-runners.helpers.js';
-
-const SEED_DATA_ROOT_PATH = '/opt';
-const SEED_DATA_FOLDER_NAME = 'app';
+import { getBookContext } from '../dals/book/book.context.js';
+import { db } from '../dals/mock-data.js';
 
 export const run = async () => {
-  const { seedDataPath, containerName, dbName } = await prompts([
-    {
-      name: 'seedDataPath',
-      type: 'text',
-      message: 'Seed data path (in your file system):',
-    },
-    {
-      name: 'containerName',
-      initial: 'mflix-db',
-      type: 'text',
-      message: 'Docker container name:',
-    },
-    {
-      name: 'dbName',
-      initial: 'mflix',
-      type: 'text',
-      message: 'Database name:',
-    },
-  ]);
-
-  const copySeedDataCommand = `docker cp "${seedDataPath}" ${containerName}:${SEED_DATA_ROOT_PATH}/${SEED_DATA_FOLDER_NAME}`;
-  const restoreBackupCommand = `docker exec ${containerName} mongorestore --nsFrom="${SEED_DATA_FOLDER_NAME}.*" --nsTo="${dbName}.*" ${SEED_DATA_ROOT_PATH}`;
-
-  await runCommand(copySeedDataCommand);
-  await runCommand(restoreBackupCommand);
+  await getBookContext().insertMany(db.books);
 };
